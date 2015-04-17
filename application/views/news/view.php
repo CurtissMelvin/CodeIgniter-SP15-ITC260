5024da259ae3a7c4546a58d3539ba9df4c94dc39
Curtiss <cmelvi02@seattlecentral.edu>
2015-04-16 17:57:36 -0700

    Edited view.php

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,15 +1,3 @@
-public function view($slug = NULL)
-{
-        $data['news_item'] = $this->news_model->get_news($slug);
-
-        if (empty($data['news_item']))
-        {
-                show_404();
-        }
-
-        $data['title'] = $data['news_item']['title'];
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('news/view', $data);
-        $this->load->view('templates/footer');
-}
+<?php
+echo '<h2>' . $news_item['title'].'</h2>';
+echo $news_item['text'];
