eab787cf7687bd8b541c13afe35d5daf6a46a9ae
Curtiss <cmelvi02@seattlecentral.edu>
2015-04-21 16:55:15 -0700

    Edited MVC files

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,11 +55,9 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 */
 
-/*
-$route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';
-*/
 
+
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
---
+++ application/views/news/success.php
@@ -0,0 +1,2 @@
+<h1>Success!!!</h1>
+<p>You have succeeded for this file</p>
